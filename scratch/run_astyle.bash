2017f8145afbc9b0fc6cd542fc20949859cd4de9
Chris Saunders <csaunders@illumina.com>
2013-03-18 15:32:50 -0700

    candidate formatting script

---
+++ scratch/run_astyle.bash
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+#
+# if asytle is found, then run it on the starka code base with minimal re-formatting options
+#
+
+set -o nounset
+
+if ! which -a astyle > /dev/null 2>&1 ; then exit 0; fi
+
+thisDir=$(dirname $0)
+
+cd $thisDir/../starka/src
+
+
+# conservative code re-formatting:
+astyle \
+--align-pointer=type \
+--keep-one-line-blocks \
+--keep-one-line-statements \
+--max-instatement-indent=80 \
+--recursive \
+*.cpp *.hh *.h
+
