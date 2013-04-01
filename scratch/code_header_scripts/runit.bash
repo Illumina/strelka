deb2802355d26832808db6cde3e0ece5b9f38d81
Chris Saunders <csaunders@illumina.com>
2013-04-01 15:33:57 -0700

    update reheader script to cover test cases

--- scratch/code_header_scripts/runit.bash
+++ scratch/code_header_scripts/runit.bash
@@ -2,18 +2,37 @@
 
 set -o nounset
 
-base_dir=../strelka_workflow/strelka
 
-for f in include lib; do
-  fdir=$base_dir/$f
-  for g in blt_common blt_util starling_common strelka; do
-    for h in $fdir/$g/*; do
-      hf=$(basename $h)
-      if [ $hf == "CVS" ]; then continue; fi
-      echo $h
-      reheader.pl new_header < $h >| foo 
-      mv foo $h
-      if [ $? != 0 ]; then echo "error on file $h"; fi
+rel2abs() {
+    (cd $1; pwd -P)
+}
+
+
+reheader_file() {
+    file=$1
+
+    if [ ! -f $file ]; then return; fi
+    echo $file
+    #reheader.pl new_header < $file >| foo 
+    #mv foo $file
+    if [ $? != 0 ]; then echo "error on file $file"; fi
+}
+
+
+thisdir=$(rel2abs $(dirname $0))
+
+base_dir=$(rel2abs $thisdir/../../starka/src)
+
+for f in lib; do
+    for g in blt_common blt_util common starling_common starling strelka; do
+        fdir=$base_dir/$f/$g
+        for h in $fdir/* $fdir/test/*; do
+            reheader_file $h
+        done
     done
-  done
 done
+
+for h in $base_dir/bin/*; do
+    reheader_file $h
+done
+
