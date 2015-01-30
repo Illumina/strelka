7404ebb1555d47f882a4db1f4233c73e7686ec4a
Chris Saunders <csaunders@illumina.com>
2015-01-30 11:10:56 -0800

    remote unused strelka files

--- src/c++/lib/applications/strelka/strelka_pos_processor.cpp
+++ src/c++/lib/applications/strelka/strelka_pos_processor.cpp
@@ -19,7 +19,6 @@
 #include "position_somatic_snv.hh"
 #include "position_somatic_snv_strand_grid.hh"
 #include "position_somatic_snv_strand_grid_vcf.hh"
-#include "somatic_indel.hh"
 #include "somatic_indel_grid.hh"
 #include "strelka_pos_processor.hh"
 
--- src/c++/lib/applications/strelka/strelka_shared.cpp
+++ src/c++/lib/applications/strelka/strelka_shared.cpp
@@ -17,7 +17,6 @@
 
 #include "position_somatic_snv.hh"
 #include "position_somatic_snv_strand_grid.hh"
-#include "somatic_indel.hh"
 #include "somatic_indel_grid.hh"
 #include "strelka_shared.hh"
 
