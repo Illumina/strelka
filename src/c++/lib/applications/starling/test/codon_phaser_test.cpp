a512f5286bb548c0c315987ff399147191def0c8
John Duddy <jduddy@illumina.com>
2015-05-27 13:28:30 -0700

    Negative tests

--- src/c++/lib/applications/starling/test/codon_phaser_test.cpp
+++ src/c++/lib/applications/starling/test/codon_phaser_test.cpp
@@ -39,6 +39,8 @@ static DIGT::index_t gt_for_base(char base)
 
 BOOST_AUTO_TEST_SUITE( codon_phaser )
 
+// positive tests
+
 BOOST_AUTO_TEST_CASE( simple_3mer )
 {
     reference_contig_segment rcs;
@@ -85,6 +87,10 @@ BOOST_AUTO_TEST_CASE( simple_3mer )
             if ((!phaser.is_in_block()) || emptyBuffer)
             {
                 phased_variant = phaser.buffer()[0].phased_alt;
+                for (auto v : phaser.buffer())
+                {
+                    std::cerr << "JED:" << v.pos << std::endl;
+                }
                 phaser.clear();
             }
         }
@@ -251,6 +257,138 @@ BOOST_AUTO_TEST_CASE( respects_phasing_window )
         }
     }
     BOOST_CHECK_EQUAL ("AGAACGGAG", phased_variant);
+}
+
+// negative tests
+BOOST_AUTO_TEST_CASE( just_one_snp )
+{
+    reference_contig_segment rcs;
+    rcs.seq() = "ACGTACGTACGTACGTACGTACGTACGTACGTACGTACGTACGTACGT";
+    pos_basecall_buffer bc_buff(rcs);
+
+    auto r1 = "ACGTACGT";
+    auto r2 = "ACGGACGT";
+    pos_t read_pos = 0;
+
+    // add 2 haplotypes of reads
+    for (int i = 0; i < 10; i++)
+        insert_read(r1, read_pos, bc_buff);
+    for (int i = 0; i < 10; i++)
+        insert_read(r2, read_pos, bc_buff);
+
+    starling_base_options opt;
+    opt.phasing_window = 3;
+
+    Codon_phaser phaser(opt, bc_buff);
+    int site_output_count = 0;
+
+    for (int i = 0; r1[i]; i++)
+    {
+        site_info si;
+        const snp_pos_info& spi(bc_buff.get_pos(read_pos + i));
+        si.init(read_pos + i, rcs.get_base(read_pos + i), spi, 30);
+        si.smod.is_covered = si.smod.is_used_covered = true;
+        si.smod.gq = si.dgt.genome.snp_qphred = si.smod.Qscore = 40;
+        si.dgt.ref_gt = base_to_id(si.ref);
+
+        if (r1[i] != r2[i])
+        {
+            si.dgt.is_snp = true;
+            si.smod.max_gt = DIGT::AG; // TODO: fix this, but doesn't seem to impact results
+        }
+        else
+        {
+            si.smod.max_gt = gt_for_base(r1[i]);
+        }
+        if (Codon_phaser::is_phasable_site(si) || phaser.is_in_block())
+        {
+            const bool emptyBuffer = phaser.add_site(si);
+            if ((!phaser.is_in_block()) || emptyBuffer)
+            {
+                for (auto phased_variant : phaser.buffer())
+                {
+                    BOOST_CHECK(!phased_variant.smod.filters.any());
+                    BOOST_CHECK(!phased_variant.smod.is_phased_region);
+                    site_output_count++;
+                }
+                phaser.clear();
+            }
+        }
+        else
+        {
+            site_output_count++;
+        }
+    }
+    BOOST_CHECK_EQUAL (site_output_count, strlen(r1));
+}
+
+BOOST_AUTO_TEST_CASE( read_break_causes_phasing_conflict )
+{
+    reference_contig_segment rcs;
+    rcs.seq() = "ACGTACGTACGTACGTACGTACGTACGTACGTACGTACGTACGTACGT";
+    pos_basecall_buffer bc_buff(rcs);
+
+    auto r1 = "ACGTACGTACGTACGT";
+    auto r1_nosnp1 = "ACGTACGT";
+    auto r1_nosnp2 = "ACGTACGT";
+    auto r2 = "ACGTACGGAGGTACGT";
+
+    pos_t read_pos = 0;
+    pos_t read_pos2 = strlen(r1) + read_pos;
+    pos_t read_pos_snp = read_pos;
+
+    // add 2 haplotypes of reads
+    for (int i = 0; i < 10; i++)
+    {
+        insert_read(r1_nosnp1, read_pos, bc_buff);
+        insert_read(r1_nosnp2, read_pos2, bc_buff);
+    }
+
+    for (int i = 0; i < 10; i++)
+    {
+        insert_read(r2, read_pos_snp, bc_buff);
+    }
+
+    starling_base_options opt;
+    opt.phasing_window = 3;
+
+    Codon_phaser phaser(opt, bc_buff);
+
+
+    for (int i = 0; r1[i]; i++)
+    {
+        site_info si;
+        const snp_pos_info& spi(bc_buff.get_pos(read_pos + i));
+        si.init(read_pos + i, rcs.get_base(read_pos + i), spi, 30);
+        si.smod.is_covered = si.smod.is_used_covered = true;
+        si.smod.gq = si.dgt.genome.snp_qphred = si.smod.Qscore = 40;
+        si.dgt.ref_gt = base_to_id(si.ref);
+
+        if (r1[i] != 'N' && r1[i] != r2[i])
+        {
+            si.dgt.is_snp = true;
+            si.smod.max_gt = DIGT::AG; // TODO: fix this, but doesn't seem to impact results
+        }
+        else
+        {
+            si.smod.max_gt = gt_for_base(r1[i]);
+        }
+        if (Codon_phaser::is_phasable_site(si) || phaser.is_in_block())
+        {
+            const bool emptyBuffer = phaser.add_site(si);
+            if ((!phaser.is_in_block()) || emptyBuffer)
+            {
+                bool first = true;
+                for (auto phased_variant : phaser.buffer())
+                {
+                    BOOST_CHECK(!first || phased_variant.smod.filters.test(VCF_FILTERS::PhasingConflict));
+                    BOOST_CHECK(!phased_variant.smod.is_phased_region);
+                    first = false;
+                }
+                phaser.clear();
+            }
+        }
+    }
 }
 
 
