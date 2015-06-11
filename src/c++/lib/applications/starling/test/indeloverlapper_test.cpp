b1562fcae10c75d91f7d22eb9e89afe5f929e234
John Duddy <jduddy@illumina.com>
2015-06-11 10:35:15 -0700

    Remove empty tests

--- src/c++/lib/applications/starling/test/indeloverlapper_test.cpp
+++
@@ -1,9 +0,0 @@
-/*
- * indeloverlapper_test.cpp
- *
- *  Created on: Jun 3, 2015
- *      Author: jduddy
- */
-
-#include "../indel_overlapper.hh"
-
--- src/c++/lib/applications/starling/test/variantprefilterstage_test.cpp
+++
@@ -1,9 +0,0 @@
-/*
- * variantprefilterstage_test.cpp
- *
- *  Created on: Jun 4, 2015
- *      Author: jduddy
- */
-
-#include "../variant_prefilter_stage.hh"
-
