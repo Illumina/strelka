56724560fb2b56c61603d217fbc9e3b309a28737
Sangtae Kim <skim2@illumina.com>
2016-02-05 10:44:04 -0800

    Misc bug fix.

--- src/c++/lib/applications/strelka/position_somatic_snv_strand_grid.cpp
+++ src/c++/lib/applications/strelka/position_somatic_snv_strand_grid.cpp
@@ -453,7 +453,7 @@ calculate_result_set_grid(
         const bool isComputeNonSomatic,
         const blt_float_t* normal_lhood,
         const blt_float_t* tumor_lhood,
-        const std::vector<blt_float_t> ln_freq_given_somatic,
+        const std::vector<blt_float_t>& ln_freq_given_somatic,
         const blt_float_t* bare_lnprior_normal,
         const blt_float_t lnmatch,
         const blt_float_t lnmismatch,
