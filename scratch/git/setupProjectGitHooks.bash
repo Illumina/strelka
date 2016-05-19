6675cd2e8114485c4c3516c3a0aadcfce802a586
Chris Saunders <csaunders@illumina.com>
2016-05-19 15:55:43 -0700

    build: add a quick git client hook initializer
    Just a simple commit message template for now, but allows
    for a standard team (& Jenkins) client-side hook disto
    going forward

---
+++ scratch/git/setupProjectGitHooks.bash
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+rel2abs() {
+  (cd $1 && pwd -P)
+}
+
+thisDir=$(rel2abs $(dirname $0))
+baseDir=$(rel2abs $thisDir/../..)
+hookDir=$baseDir/.git/hooks
+
+hook1=$hookDir/prepare-commit-msg
+
+if [ -e $hook1 ]; then
+    mv $hook1 $hook1.$$.manta_backup
+fi
+
+cat << END > $hook1
+#!/usr/bin/env bash
+#
+
+# this adds the branch name to the beginning of the commit message
+BRANCH_NAME=\$(git rev-parse --abbrev-ref HEAD)
+FIRST_LINE=\$(head -1 \$1)
+if [ -z \$FIRST_LINE ] && [ "X\$BRANCH_NAME" != "Xmaster" ] ; then
+    sed -i "1s/^/\$BRANCH_NAME \n/" \$1
+fi
+END
+
+chmod +x $hook1
+
