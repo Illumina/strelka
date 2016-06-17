d5b97b01b577bf305fbe2ec0bffb27e4cd05db59
Mitch Bekritsky <mbekritsky@illumina.com>
2016-06-17 10:28:52 +0100

    STARKA-371: Updated vcf_record::is_left_shifted to vcf_record::is_normalized, now checks that MNVs, SNVs, and indels are normalized.

--- src/c++/lib/htsapi/test/vcf_streamer_test.cpp
+++ src/c++/lib/htsapi/test/vcf_streamer_test.cpp
@@ -48,10 +48,10 @@ BOOST_AUTO_TEST_CASE( test_vcf_streamer_region )
 
     // VCF record test for
     // chr1    757807  MantaDEL:44:0:0:0:1:0   CCCTGGCCAGCAGATCCACCCTGTCTATACTACCTG    C       ...
-    // testing variant assignment, left-shifting, validity, position, and ref
+    // testing variant assignment, normalize and validity checks, position, and ref
     BOOST_REQUIRE( vptr->is_valid() );
     BOOST_REQUIRE( vptr->is_indel() );
-    BOOST_REQUIRE( vptr->is_left_shifted());
+    BOOST_REQUIRE( vptr->is_normalized());
     BOOST_REQUIRE( ! vptr->is_snv() );
 
     BOOST_REQUIRE_EQUAL(vptr->pos, 757807);
@@ -63,11 +63,11 @@ BOOST_AUTO_TEST_CASE( test_vcf_streamer_region )
 
     // VCF record test for
     // chr1    758807  FAKED   C       T,A     ...
-    // testing variant assignment, left-shifting, validity, position, alt size, and alt
+    // testing variant assignment, normalize and validity checks, position, alt size, and alt
     BOOST_REQUIRE( vptr->is_valid() );
     BOOST_REQUIRE( ! vptr->is_indel() );
     BOOST_REQUIRE( vptr->is_snv() );
-    BOOST_REQUIRE( vptr->is_left_shifted());
+    BOOST_REQUIRE( vptr->is_normalized());
 
     BOOST_REQUIRE_EQUAL(vptr->pos, 758807);
     BOOST_REQUIRE_EQUAL(vptr->alt.size(),2u);
@@ -79,11 +79,11 @@ BOOST_AUTO_TEST_CASE( test_vcf_streamer_region )
 
     // VCF record test for
     // chr1    821604  MantaINS:53:0:0:0:1:0   T       TGCCCTTTGGCAGAGCAGGTGTGCTGTGCTG ...
-    // testing variant assignment, left-shifting, validity, position, alt size, and alt
+    // testing variant assignment, normalize and validity checks, position, alt size, and alt
     BOOST_REQUIRE( vptr->is_valid() );
     BOOST_REQUIRE( vptr->is_indel() );
     BOOST_REQUIRE( ! vptr->is_snv() );
-    BOOST_REQUIRE( vptr->is_left_shifted());
+    BOOST_REQUIRE( vptr->is_normalized());
 
     BOOST_REQUIRE_EQUAL(vptr->pos, 821604);
     BOOST_REQUIRE_EQUAL(vptr->alt.size(),1u);
@@ -98,11 +98,11 @@ BOOST_AUTO_TEST_CASE( test_vcf_streamer_region )
 
     // VCF record test for
     // chr10   89717769        COSM30622       TA      T       ...
-    // testing that deletion is reported as properly left-shifted
+    // testing that deletion is reported as normalized
     BOOST_REQUIRE( vptr->is_valid() );
     BOOST_REQUIRE( vptr->is_indel() );
     BOOST_REQUIRE( ! vptr->is_snv() );
-    BOOST_REQUIRE( vptr->is_left_shifted());
+    BOOST_REQUIRE( vptr->is_normalized());
     BOOST_REQUIRE_EQUAL(vptr->pos, 89717769);
     BOOST_REQUIRE_EQUAL(vptr->alt.size(),1u);
     BOOST_REQUIRE_EQUAL(vptr->alt[0],"T");
@@ -117,7 +117,7 @@ BOOST_AUTO_TEST_CASE( test_vcf_streamer_region )
     BOOST_REQUIRE( vptr->is_valid() );
     BOOST_REQUIRE( vptr->is_indel() );
     BOOST_REQUIRE( ! vptr->is_snv() );
-    BOOST_REQUIRE( ! vptr->is_left_shifted());
+    BOOST_REQUIRE( ! vptr->is_normalized());
     BOOST_REQUIRE_EQUAL(vptr->pos, 89717774);
     BOOST_REQUIRE_EQUAL(vptr->alt.size(),1u);
     BOOST_REQUIRE_EQUAL(vptr->alt[0],"A");
@@ -132,10 +132,85 @@ BOOST_AUTO_TEST_CASE( test_vcf_streamer_region )
     BOOST_REQUIRE( vptr->is_valid() );
     BOOST_REQUIRE( vptr->is_indel() );
     BOOST_REQUIRE( ! vptr->is_snv() );
-    BOOST_REQUIRE( ! vptr->is_left_shifted());
+    BOOST_REQUIRE( ! vptr->is_normalized());
     BOOST_REQUIRE_EQUAL(vptr->pos, 89717775);
     BOOST_REQUIRE_EQUAL(vptr->alt.size(),1u);
     BOOST_REQUIRE_EQUAL(vptr->alt[0],"AA");
+
+    BOOST_REQUIRE( vcfs.next() );
+    vptr = vcfs.get_record_ptr();
+    assert(vptr != nullptr);
+
+    // VCF record test for
+    // chr10   89717776        VALID_MNV_1     TCG     AGT     ...
+    // testing that MNV is reported as normalized
+    BOOST_REQUIRE( vptr->is_valid() );
+    BOOST_REQUIRE( vptr->is_indel() );
+    BOOST_REQUIRE( ! vptr->is_snv() );
+    BOOST_REQUIRE( vptr->is_normalized());
+    BOOST_REQUIRE_EQUAL(vptr->pos, 89717776);
+    BOOST_REQUIRE_EQUAL(vptr->alt.size(),1u);
+    BOOST_REQUIRE_EQUAL(vptr->alt[0],"AGT");
+
+    BOOST_REQUIRE( vcfs.next() );
+    vptr = vcfs.get_record_ptr();
+    assert(vptr != nullptr);
+
+    // VCF record test for
+    // chr10   89717779        INVALID_MNV_1   TCG     AGG     ...
+    // testing that MNV is not reported as normalized
+    BOOST_REQUIRE( vptr->is_valid() );
+    BOOST_REQUIRE( vptr->is_indel() );
+    BOOST_REQUIRE( ! vptr->is_snv() );
+    BOOST_REQUIRE( ! vptr->is_normalized());
+    BOOST_REQUIRE_EQUAL(vptr->pos, 89717779);
+    BOOST_REQUIRE_EQUAL(vptr->alt.size(),1u);
+    BOOST_REQUIRE_EQUAL(vptr->alt[0],"AGG");
+
+    BOOST_REQUIRE( vcfs.next() );
+    vptr = vcfs.get_record_ptr();
+    assert(vptr != nullptr);
+
+    // VCF record test for
+    // chr10   89717782        INVALID_MNV_2   ACG     AGT     ...
+    // testing that MNV is not reported as normalized
+    BOOST_REQUIRE( vptr->is_valid() );
+    BOOST_REQUIRE( vptr->is_indel() );
+    BOOST_REQUIRE( ! vptr->is_snv() );
+    BOOST_REQUIRE( ! vptr->is_normalized());
+    BOOST_REQUIRE_EQUAL(vptr->pos, 89717782);
+    BOOST_REQUIRE_EQUAL(vptr->alt.size(),1u);
+    BOOST_REQUIRE_EQUAL(vptr->alt[0],"AGT");
+
+    BOOST_REQUIRE( vcfs.next() );
+    vptr = vcfs.get_record_ptr();
+    assert(vptr != nullptr);
+
+    // VCF record test for
+    // chr10   89717785        INVALID_SNV   A     A     ...
+    // testing that SNV is not reported as normalized
+    BOOST_REQUIRE( vptr->is_valid() );
+    BOOST_REQUIRE( ! vptr->is_indel() );
+    BOOST_REQUIRE( vptr->is_snv() );
+    BOOST_REQUIRE( ! vptr->is_normalized());
+    BOOST_REQUIRE_EQUAL(vptr->pos, 89717785);
+    BOOST_REQUIRE_EQUAL(vptr->alt.size(),1u);
+    BOOST_REQUIRE_EQUAL(vptr->alt[0],"A");
+
+    BOOST_REQUIRE( vcfs.next() );
+    vptr = vcfs.get_record_ptr();
+    assert(vptr != nullptr);
+
+    // VCF record test for
+    // chr10   89717785        RIGHT_PAD_INDEL ACCC    AC      ...
+    // testing that indel is not reported as normalized
+    BOOST_REQUIRE( vptr->is_valid() );
+    BOOST_REQUIRE( vptr->is_indel() );
+    BOOST_REQUIRE( ! vptr->is_snv() );
+    BOOST_REQUIRE( ! vptr->is_normalized());
+    BOOST_REQUIRE_EQUAL(vptr->pos, 89717785);
+    BOOST_REQUIRE_EQUAL(vptr->alt.size(),1u);
+    BOOST_REQUIRE_EQUAL(vptr->alt[0],"AC");
 
     // testing that next returns false after last valid variant
     // (test file has an invalid variant at the end that should be
