9cc7f985e9e3e226a1722363c9638f2afef9a02b
Chris Saunders <csaunders@illumina.com>
2016-07-15 15:21:47 -0700

    STREL-163 improve EVS model util script

--- scratch/util/mergeScoringModels.py
+++ scratch/util/mergeScoringModels.py
@@ -18,11 +18,35 @@ def update(d, u):
             d[k] = u[k]
     return d
 
-data = {} 
-for infile in sys.argv[1:] :
-    sys.stderr.write("starting file: %s\n" % (infile))
-    indata = json.load(open(infile))
-    update(data, indata)
+def getOptions() :
 
-json.dump(data,sys.stdout)
+    from optparse import OptionParser
+
+    usage = "usage: %prog [file1.json [file2.json [...]]] > out.json"
+    description="""
+merge json files
+"""
+    parser = OptionParser(usage=usage)
+
+    (opt,args) = parser.parse_args()
+
+    if len(args) == 0 :
+        parser.print_help()
+        sys.exit(2)
+
+    return (opt,args)
+
+def main() :
+
+    (opt,args) = getOptions()
+
+    data = {}
+    for infile in args :
+        sys.stderr.write("starting file: %s\n" % (infile))
+        indata = json.load(open(infile))
+        update(data, indata)
+
+    json.dump(data,sys.stdout)
+
+main()
 
