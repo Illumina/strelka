9a6276ee3a70b81142a97a93e7a2a91f9b75c8fd
Eunho Noh <enoh@illumina.com>
2017-05-04 10:28:31 -0700

    STREL-486 Add production estimator executable
    EstimateVariantErrorRates has been added to be used in production.

---
+++ src/c++/lib/applications/EstimateVariantErrorRates/EstimateVariantErrorRates.cpp
@@ -0,0 +1,53 @@
+//
+// Strelka - Small Variant Caller
+// Copyright (c) 2009-2017 Illumina, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+//
+
+
+#include <iostream>
+
+#include "EstimateVariantErrorRates.hh"
+#include "EPECOptions.hh"
+#include "indelModelProduction.hh"
+
+
+
+
+static
+void
+runEPEC(
+    const EPECOptions& opt)
+{
+    SequenceErrorCounts counts;
+    counts.load(opt.countsFilename.c_str());
+
+
+    indelModelProduction(counts, opt.thetaFilename, opt.outputFilename);
+
+}
+
+
+
+void
+EstimateVariantErrorRates::
+runInternal(int argc, char* argv[]) const
+{
+    EPECOptions opt;
+
+    parseEPECOptions(*this,argc,argv,opt);
+    runEPEC(opt);
+}
---
+++ src/c++/lib/applications/EstimateVariantErrorRates/EstimateVariantErrorRates.hh
@@ -0,0 +1,36 @@
+//
+// Strelka - Small Variant Caller
+// Copyright (c) 2009-2017 Illumina, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+//
+
+#pragma once
+
+#include "common/Program.hh"
+
+
+struct EstimateVariantErrorRates : public illumina::Program
+{
+    const char*
+    name() const
+    {
+        return "EstimateVariantErrorRates";
+    }
+
+    void
+    runInternal(int argc, char* argv[]) const;
+};
+
