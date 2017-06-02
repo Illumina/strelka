6f918da5561c98bd63ec203569a4a34b7e356915
Chris Saunders <csaunders@illumina.com>
2017-06-02 14:23:46 -0700

    STREL-576 Update demo coordinate system
    Transform the demo data into a new subset of chr20 designed to
    include the same test data but a much smaller installation
    size.

--- src/demo/runStrelkaGermlineWorkflowDemo.bash
+++ src/demo/runStrelkaGermlineWorkflowDemo.bash
@@ -62,9 +62,9 @@ END
 fi
 
 cmd="$configScript \
---bam='$dataDir/NA12891_dupmark_chr20_region.bam' \
---bam='$dataDir/NA12892_dupmark_chr20_region.bam' \
---referenceFasta='$dataDir/chr20_860k_only.fa' \
+--bam='$dataDir/NA12891_demo20.bam' \
+--bam='$dataDir/NA12892_demo20.bam' \
+--referenceFasta='$dataDir/demo20.fa' \
 --callMemMb=1024 \
 --exome \
 --disableSequenceErrorEstimation \
--- src/demo/runStrelkaSomaticWorkflowDemo.bash
+++ src/demo/runStrelkaSomaticWorkflowDemo.bash
@@ -59,9 +59,9 @@ END
 fi
 
 cmd="$configScript \
---tumorBam='$dataDir/NA12891_dupmark_chr20_region.bam' \
---normalBam='$dataDir/NA12892_dupmark_chr20_region.bam' \
---referenceFasta='$dataDir/chr20_860k_only.fa' \
+--tumorBam='$dataDir/NA12891_demo20.bam' \
+--normalBam='$dataDir/NA12892_demo20.bam' \
+--referenceFasta='$dataDir/demo20.fa' \
 --callMemMb=1024 \
 --exome \
 --runDir=$analysisDir"
